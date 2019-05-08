a87ecf5c55fcbb2c0fa7e5506cb9ac64fb1d4752
Dhruv <dhruvpatel5738@gmail.com>
2019-05-07 20:13:30 -0700

    Fix #79

--- src/my-about.js
+++ src/my-about.js
@@ -1,8 +1,10 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import '@polymer/paper-card/paper-card.js'
 import '@polymer/iron-flex-layout/iron-flex-layout.js'
+import '@polymer/paper-tooltip/paper-tooltip.js';
 import './my-icons.js'
 import './shared-styles.js'
+import './paper-chip.js';
 
 
 class MyAbout extends PolymerElement {
@@ -51,9 +53,30 @@ class MyAbout extends PolymerElement {
           width:50%;
           height:auto
         }
+
+        paper-chip.custom-background {
+          --paper-chip-background-color: #dc3545;
+          --paper-chip-label-color: #fff;
+        }
+        paper-chip.custom-background-m {
+          --paper-chip-background-color: #007bff;
+          --paper-chip-label-color: #fff;
+        }
+
+        paper-tooltip {
+          text-align: left;
+          font-size: 30px;
+        }
+
+        paper-tooltip.giant {
+        --paper-tooltip: {
+          font-size: 14px;
+          width: 40%;
+        }
+      }
     </style>
 
-    <center><h1>What is MINT?</h1></center>
+    <center><h1>What is MINT?<paper-chip label="v 0.0.2" class="custom-background-m" id="btn"></paper-chip><paper-tooltip class="giant" for="btn">This release consists of the following features:<br><ul><li>Adding Causal Diagrams using D3.js with zoom, drag and node highlighting.</li><br><li>View Model Page now has more detailed information regarding input output files.</li><br><li>UI Improvements to View Model Page.</li></ul></paper-tooltip></h1></center>
     <center>
       Major societal and environmental challenges require forecasting how natural processes and human activities affect one another. There are many areas of the globe where climate affects water resources and therefore food availability, with major economic and social implications. Today, such analyses require significant effort to integrate highly heterogeneous models from separate disciplines, including geosciences, agriculture, economics, and social sciences. Model integration requires resolving semantic, spatio-temporal, and execution mismatches, which are largely done by hand today and may take more than two years. The Model INTegration (MINT) project will develop a modeling environment which will significantly reduce the time needed to develop new integrated models, while ensuring their utility and accuracy.
     </center>
